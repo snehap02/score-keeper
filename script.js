aa2070a604f3c9757fe5990dc9ba6278b0213f79
Sneha Purkayastha <iamsneha0296@gmail.com>
2023-01-06 17:54:29 +0530

    final commit

--- script.js
+++ script.js
@@ -15,10 +15,13 @@ btn1.addEventListener('click', () => {
         firstScore += 1;
         if (firstScore === goalCount) {
             isGameOver = true;
-            player1.classList.add('winner');
-            player2.classList.add('loser');
-            btn1.disabled = true;
-            btn2.disabled = true;
+            player1.classList.add('text-green-600');
+            player2.classList.add('text-red-600');
+            btn1.classList.add('cursor-not-allowed');
+            btn2.classList.add('cursor-not-allowed');
+            btn1.classList.add('opacity-50');
+            btn2.classList.add('opacity-50');
+
         }
         player1.textContent = firstScore;
     }
@@ -29,10 +32,13 @@ btn2.addEventListener('click', () => {
         secondScore += 1;
         if (secondScore === goalCount) {
             isGameOver = true;
-            player2.classList.add('winner');
-            player1.classList.add('loser');
-            btn1.disabled = true;
-            btn2.disabled = true;
+            player2.classList.add('text-green-600');
+            player1.classList.add('text-red-600');
+            btn1.classList.add('cursor-not-allowed');
+            btn2.classList.add('cursor-not-allowed');
+            btn1.classList.add('opacity-50');
+            btn2.classList.add('opacity-50');
+
         }
         player2.textContent = secondScore;
     }
@@ -52,10 +58,12 @@ function resetGame(){
     secondScore = 0;
     player1.textContent = 0;
     player2.textContent = 0;
-    player1.classList.remove('winner', 'loser');
-    player2.classList.remove('winner', 'loser');
-    btn1.disabled = false;
-    btn2.disabled = false;
+    player1.classList.remove('text-green-600', 'text-red-600');
+    player2.classList.remove('text-green-600', 'text-red-600');
+    btn1.classList.remove('cursor-not-allowed');
+    btn2.classList.remove('cursor-not-allowed');
+    btn1.classList.remove('opacity-50');
+    btn2.classList.remove('opacity-50');
 }
 
 
